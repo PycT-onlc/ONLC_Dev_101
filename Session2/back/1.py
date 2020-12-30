f65c40901b98caa2fd1306c41ee0a8dcb13d7869
Rustem Zakiev <rustem.zakiev@gmail.com>
2020-12-30 16:37:20 +0300

    connected front to back

--- Session2/back/1.py
+++ Session2/back/1.py
@@ -1,14 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def myfront():
+
+	return render_template("index.html")
+
+
+@app.route("/back", methods = ["GET", "POST"])
 def index():
 
 	print(request.method)
 	print(request.data)  # все данные из post-запроса тут.
 	print(request.values)  # все данные из get-запроса либо переданные post'ом из формы тут 
+
 	return "<h1>hi</h1><br>{}<hr>{}".format(request.method, request.values)
 
 app.run(debug = True, host = "localhost", port = "3000")
+
