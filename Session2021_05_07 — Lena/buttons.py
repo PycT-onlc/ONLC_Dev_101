71a94a6a031a02840237f3e479b2c1a7616a4dff
Лямина Елена Владиславовна <efedorova@mx.one>
2021-05-07 16:20:24 +0300

    add Lena folder3

---
+++ Session2021_05_07 — Lena/buttons.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request;
+
+app = Flask(__name__);
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html");
+
+    def main():
+
+    app.run(debug = False, host = "0.0.0.0", port = "5001");
+
+    return True;
+
+if __name__ == "__main__":
+    main();
